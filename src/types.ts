614d14043403212a5180645d4778673c43e587c8
Tom <46662098+TomRome2Rio@users.noreply.github.com>
2026-04-12 16:01:40 +1000

    Revert "Memberships: Move to rolling 12 months (#43)" (#45)
    This reverts commit 873226fcf3806b2138563922a5b7992ad129df4f.

--- src/types.ts
+++ src/types.ts
@@ -49,7 +49,6 @@ export interface User {
   isMember: boolean;
   isAdmin?: boolean;
   membershipPaidDate?: string; // ISO date string (YYYY-MM-DD) of when membership was last paid
-  membershipExpiryDate?: string; // ISO date string (YYYY-MM-DD) of when membership expires
 }
 
 export interface MembershipAuditEntry {
